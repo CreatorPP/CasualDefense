c1a1dc9c340b78db906fcd8e5364115eff0532ce
CreatorPP <86832002+CreatorPP@users.noreply.github.com>
2023-04-07 22:42:34 +0900

    Unity Bolt Casual Defense
    유니티 Bolt를 이용해 만든 Casual Defense

---
+++ Assets/Scripts/MyType.cs
@@ -0,0 +1,7 @@
+﻿
+public class MyType
+{
+
+    public enum Unity { Sword, Range, Guard, Wizard, Bullet }
+    public enum Sound {  Victory, Defeat, Buy, Upgrade, Sword, Range, RangeHit, Guard, Magic}
+}
